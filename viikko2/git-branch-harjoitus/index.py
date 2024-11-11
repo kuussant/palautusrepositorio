1bf065f16733cb40f1efe5dc5305ae9a06a9b3a7
kuussant <santeri.kuusisto@helsinki.fi>
2024-11-11 20:14:10 +0200

    bugikorjaus

--- viikko2/git-branch-harjoitus/index.py
+++ viikko2/git-branch-harjoitus/index.py
@@ -11,6 +11,21 @@ y = int(input("luku 2: "))
 print(f"{summa(x, y)}")
 print(f"{erotus(x, y)}")
 
+logger("lopetetaan ohjelma")
+print("goodbye!") # lisäys bugikorjaus-branchissa
+# tehdään alussa importit
+
+from logger import logger
+from summa import summa
+from erotus import erotus
+
+logger("aloitetaan")
+
+x = int(input("luku 1: "))
+y = int(input("luku 2: "))
+print(f"{summa(x, y)}")
+print(f"{erotus(x, y)}")
+
 logger("lopetetaan")
 from logger import logger
 from summa import summa
